cad6518b2164787584f6d9617a9f6628d987a46e
Никита Шалимов <78145635+Nikitashalimov@users.noreply.github.com>
2023-11-04 22:16:29 +0500

    Update resultSearchModule.js

--- src/store/resultSearchModule.js
+++ src/store/resultSearchModule.js
@@ -12,32 +12,32 @@ export const resultSearchModule = {
 			state.books = books;
 		},
 		removeBooks(state) {
-			state.books = [];
+			if (state.books.length > 0) {
+				state.books = [];
+			}
 		},
 	},
 	actions: {
 		async fetchBooks(context, searchKeyWord) {
-			if (!searchKeyWord) {
-				context.commit('removeBooks');
-			} else {
-				
-				try {
-					const res = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchKeyWord}`);
+			context.commit('removeBooks');
 
-					if (!res.ok) {
-						throw new Error("Failed to fetch books.");
-					}
 
-					const data = await res.json();
+			try {
+				const res = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchKeyWord}`);
 
-					if (data.items) {
-						context.commit('setBooks', data.items);
-					} else {
-						throw new Error("No books found.");
-					}
-				} catch (error) {
-					console.error("Error fetching books:", error);
+				if (!res.ok) {
+					throw new Error("Failed to fetch books.");
 				}
+
+				const data = await res.json();
+
+				if (data.items) {
+					context.commit('setBooks', data.items);
+				} else {
+					throw new Error("No books found.");
+				}
+			} catch (error) {
+				console.error("Error fetching books:", error);
 			}
 		}
 	}
