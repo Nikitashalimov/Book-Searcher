48691d775d8a99d1054787f682538e6a8daa8db3
Никита Шалимов <78145635+Nikitashalimov@users.noreply.github.com>
2023-11-05 18:55:50 +0500

    style loginView, style header, add fonts

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,9 @@
 <template>
-  <header>
-    <h1>Book researcher</h1>
-    <div>
-      <span>user:</span>
-      <h3>{{ getUserToken }}</h3>
-      <button @click="logout">exit</button>
+  <header class="header">
+    <h1 class="header_logo">Book researcher</h1>
+    <div v-if="getUserToken" class="header_login_block">
+      <h3 class="user_token">{{ getUserToken }}</h3>
+      <button class="button_logout" @click="logout">Exit</button>
     </div>
   </header>
 </template>
@@ -29,4 +28,73 @@ export default {
 
 
 <style>
+@media (max-width: 767px) {
+  .header {
+    flex-direction: column-reverse;
+  }
+
+  .header_logo {
+    margin-right: auto;
+  }
+
+  .header_login_block {
+    width: 100%;
+    justify-content: space-between;
+  }
+}
+
+@media (min-width: 768px) {
+  .header {
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+
+.header {
+  display: flex;
+  margin-bottom: 2rem;
+}
+
+.header_logo {
+  font-size: 4rem;
+  font-family: Bold_fonts;
+  color: var(--text-color);
+}
+
+.header_login_block {
+  display: flex;
+}
+
+.user_token {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 1rem;
+  font-size: 1.6rem;
+  font-family: Main_fonts;
+  color: var(--text-color);
+}
+
+.button_logout {
+  font-size: 1.6rem;
+  font-family: Main_fonts;
+  width: 6rem;
+  height: 3.5rem;
+  background-color: var(--primary-color);
+  border: 3px solid var(--outline-color);
+  border-radius: 1rem;
+  box-sizing: border-box;
+}
+
+.button_logout:hover {
+  cursor: pointer;
+  border-width: 4px;
+  background-color: var(--secondary-color);
+}
+
+.button_logout:active {
+  font-size: 1.4rem;
+  border-width: 4px;
+  background-color: var(--accent-color);
+}
 </style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,18 @@
 <template>
   <main>
-    <h2>Login</h2>
-    <input
-      type="text"
-      @keyup.enter="login"
-      v-model="newToken"
-      placeholder="Enter your userToken"
-    />
-    <button @click="login">Log In</button>
+    <form @submit.prevent="submit" class="form_login" v-if="!isLoading">
+      <input
+        ref="tokenInput"
+        class="form_login_input"
+        type="text"
+        @keyup.enter="login"
+        v-model="newToken"
+        placeholder="Your token (16 Latin characters only)"
+      />
+      <button class="form_login_button" @click="login">Log In</button>
+    </form>
+
+    <div class="spinner" v-if="isLoading"></div>
   </main>
 </template>
 
@@ -19,6 +24,7 @@ export default {
   data() {
     return {
       newToken: "",
+      isLoading: false,
     };
   },
   computed: {
@@ -30,13 +36,19 @@ export default {
       const tokenPattern = /^[A-Za-z]{16}$/;
 
       if (tokenPattern.test(this.newToken)) {
-        this.setUserToken(this.newToken);
-        console.log(`Логин сохранен в LocalStorage ${this.newToken}`);
-
-        this.$router.replace("/");
+        this.isLoading = true;
+        setTimeout(() => {
+          this.setUserToken(this.newToken);
+          this.isLoading = false;
+          this.$router.replace("/");
+        }, 3000);
       } else {
-        console.log(this.newToken);
-        console.log("Токен неверного формата");
+        this.newToken = "";
+        this.$refs.tokenInput.classList.add("invalid-token");
+        setTimeout(() => {
+          this.$refs.tokenInput.classList.remove("invalid-token");
+        }, 400);
+        return;
       }
     },
   },
@@ -50,4 +62,101 @@ export default {
 
 
 <style>
+.form_login {
+  margin-top: 6rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: Main_fonts;
+}
+
+.form_login_input,
+.form_login_button {
+  width: 30rem;
+  height: 3.2rem;
+  border: 1px solid;
+  border-radius: 1.5rem;
+  border-color: var(--outline-color);
+  background-color: var(--primary-color);
+  text-align: center;
+  font-size: 1.6rem;
+}
+
+.form_login_input {
+  outline: none;
+}
+
+.form_login_input:focus {
+  cursor: pointer;
+  background-color: var(--secondary-color);
+}
+
+.invalid-token {
+  animation: shake 0.4s ease-in-out;
+}
+
+.invalid-token::placeholder {
+  color: var(--accent-color);
+}
+
+@keyframes shake {
+  0%,
+  100% {
+    transform: translateX(0);
+    font-size: 1.6rem;
+  }
+  25% {
+    transform: translateX(-2px);
+    font-size: 1.66rem;
+  }
+  50% {
+    transform: translateX(2px);
+    font-size: 1.66rem;
+  }
+  75% {
+    transform: translateX(-2px);
+    font-size: 1.66rem;
+  }
+}
+
+.form_login_button {
+  margin-top: 1rem;
+  position: relative;
+}
+
+.form_login_button:hover {
+  cursor: pointer;
+  background-color: var(--secondary-color);
+}
+
+.form_login_button:active {
+  cursor: pointer;
+  background-color: var(--accent-color);
+  font-size: 1.4rem;
+}
+
+.spinner {
+  content: "";
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50%;
+  border-left: 5px solid var(--secondary-color);
+  border-top: 5px solid var(--secondary-color);
+  border-right: 5px solid transparent;
+  border-bottom: 5px solid var(--secondary-color);
+  margin-top: 10rem;
+  margin-left: auto;
+  margin-right: auto;
+  animation: spinner 0.8s linear infinite;
+}
+
+@keyframes spinner {
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+}
 </style>
